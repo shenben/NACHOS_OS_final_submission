0c5d65db5e7f1493b74c77c77a8c91711cce3410
Your Name <you@example.com>
2015-07-23 09:55:58 -0700

    latest from Landon

---
+++ NACHOS/nachos/test/test4.c
@@ -0,0 +1,25 @@
+/* testBlank.c
+ *
+ *	
+ *	Just do a series of syscalls that creates an empty file.
+ * 		then writes to it
+ * 			then reads(prints)
+ *
+ */
+
+#include "syscall.h"
+
+ 
+int main()
+{
+	//input parameter: char *name
+		char *myFile = "name1";
+	    //int name= 2;
+	     creat(myFile);
+
+
+	     //int unlink(char *name);
+	     unlink(myFile);
+
+	    /* not reached */
+}
